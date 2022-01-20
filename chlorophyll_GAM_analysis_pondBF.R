f13a8d9137180b67cf779f2b6c194617360702b4
Grace Wilkinson <grace.wilkinson23@gmail.com>
2022-01-19 18:34:45 -0600

    GAM for chlorophyll time series in pond B and F to establish the change in light climate associated with the nutrient pulses

---
+++ chlorophyll_GAM_analysis_pondBF.R
@@ -0,0 +1,62 @@
+# ========= PACKAGES ========== #
+if (!require(mgcv)) install.packages('mgcv')
+library(mgcv)
+if (!require(tidyverse)) install.packages('tidyverse')
+library(tidyverse)
+
+#=============================================================
+pondB_chl = read_csv("Field_Data_Processing/hort20_surface_dat.csv") %>%
+  filter(pond_id == 'B') %>%
+  filter(!(is.na(chla)))
+
+pondF_chl = read_csv("Field_Data_Processing/hort20_surface_dat.csv") %>%
+  filter(pond_id == 'F') %>%
+  filter(!(is.na(chla)))
+
+#Chlorophyll GAM - Pond B
+chlB_gam <- gam(chla ~ s(doy, k = 40), 
+               data = pondB_chl, method = 'REML')
+summary(chlB_gam)
+gam.check(chlB_gam)
+
+
+#Chlorophyll GAM - Pond F
+chlF_gam <- gam(chla ~ s(doy, k = 40), 
+                data = pondF_chl, method = 'REML')
+summary(chlF_gam)
+gam.check(chlF_gam)
+
+
+#Plotting Colors
+pondB_col_transparent = rgb(215,48,39, max = 255, alpha = 150)
+pondB_col = rgb(215,48,39, max = 255, alpha = 200)
+pondF_col_transparent = rgb(69,117,180, max = 255, alpha = 150)
+pondF_col = rgb(69,117,180, max = 255, alpha = 200)
+
+#============================
+# Set up the Plot
+windows(height = 5, width = 6.5)
+par(omi = c(0.5,0.5,0.1,0.1), mai = c(0.5,0.5,0.1,0.1))
+
+#Plot of the chlorophyll GAM for POND B ===============
+plot(chlB_gam, select = 1, 
+     seWithMean = TRUE, shift = coef(chlB_gam)[1],
+     se = TRUE, residuals = TRUE, all.terms = TRUE, shade = TRUE, rug = FALSE,
+     shade.col = pondB_col_transparent, 
+     cex = 0.75, pch = 19, lwd = 2, col = pondB_col,
+     xlab = "", ylab = "", cex.axis= 1.2, ylim = c(0,12))
+text(150, 29, "edf = 25.587***", col = "gray40", cex = 1.2, font = 4)
+mtext(side = 2, line = 3, "Chlorophyll a (ug/L)", cex = 1.25)
+mtext(side = 1, line = 3, "Day of Year, 2020", cex = 1.25)
+legend("topright", legend = c("Nutrient Addition Pond", "Reference Pond"), 
+       pch = 15, pt.cex = 2, col = c(pondB_col, pondF_col))
+
+#Plot of the chlorophyll GAM for POND F ===============
+par(new = TRUE) #add new smooth to the same plot
+plot(chlF_gam, select = 1,  
+     seWithMean = TRUE, shift = coef(chlF_gam)[1],
+     se = TRUE, residuals = TRUE, all.terms = TRUE, shade = TRUE, rug = FALSE,
+     shade.col = pondF_col_transparent, 
+     cex = 0.75, pch = 19, lwd = 2, col = pondF_col,
+     main = "", xlab = "", ylab = "",
+     xaxt = "n", yaxt = "n")
